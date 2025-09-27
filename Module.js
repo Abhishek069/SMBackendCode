23cf5a4b17a16390f174e79d61cf974409809431
Abhishek069 <abhishinde0723@gmail.com>
2025-09-28 00:09:32 +0530

    update name

--- Module.js
+++ Module.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true },
     role: { type: String, required: true },
+    name: { type: String, required: true, unique: true },
     mobile: { type: String, required: true },
     password: { type: String, required: true },
     address: { type: String, required: true },
